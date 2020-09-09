1af4a20233a4625de3684a243633be3afddf09d7
slashformotion <slashformotion@protonmail.com>
2020-09-09 21:37:35 +0200

    import system changed

--- ENIB_lib/__init__.py
+++ ENIB_lib/__init__.py
@@ -1,8 +1,2 @@
 # import of filter models
 from .core import General_Second_Order, LP, HP, BP, Notch
-
-# functions to plot bodes, time related function and poles/zeros
-from .plot import *
-
-# export csv func from an array
-from .tools import *
