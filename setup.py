6048b3a71c9c6b600129f83f57fa163df9842c74
slashformotion <slashformotion@protonmail.com>
2020-09-11 22:23:20 +0200

    setting setup

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import setup, find_packages
 
 VERSION = "0.0.1"
 
-tests_require = ["unitest", "sphinx"]
+tests_require = ["unittest", "sphinx"]
 
-requires = ["numpy", "mathplotlib", "scipy"]
+requires = ["numpy", "matplotlib", "scipy"]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="",
+    name="SecondOrderElec",
     version=VERSION,
-    url="",
+    url="https://github.com/slashformotion/SecondOrderElec",
     license="MIT",
     author="slashformotion",
     author_email="slashformotion@protonmail.com",
-    description="",
-    packages=[""],
+    description="A set of tools to work with continuous-time system. You will find various second order electronic filters models.",
+    packages=["SecondOrderElec"],
     long_description=long_description,
     long_description_content_type="text/markdown",
     tests_require=tests_require,
@@ -29,7 +29,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.9",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
