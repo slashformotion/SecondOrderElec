167b94eb554ca173e1f7f88415f77e4c9564ad45
slashformotion <slashformotion@protonmail.com>
2020-09-11 23:13:31 +0200

    pass

--- SecondOrderElec/tools.py
+++ SecondOrderElec/tools.py
@@ -5,3 +5,7 @@ def export_csv(data_list, filename="data.csv", header=""):
     data_2D = np.vstack(data_list)
     data_2D = np.transpose(data_2D)
     np.savetxt(filename, data_2D, delimiter=",", header=header)
+
+
+def test():
+    pass
