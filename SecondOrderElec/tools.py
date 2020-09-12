e5de273567e036fdb0a308b393cc6ff7980d9a80
slashformotion <slashformotion@protonmail.com>
2020-09-12 18:14:52 +0200

    add doc for tools.py and - - .tools.export_csv() func

--- SecondOrderElec/tools.py
+++ SecondOrderElec/tools.py
@@ -6,11 +6,17 @@
 import numpy as np
 
 
-def export_csv(data_list, filename="data.csv", header=""):
+def export_csv(data_list, filename="data.csv", header="", delimiter=","):
+    """export csv data to file
+
+    Args:
+        data_list (dataframe): the data to store
+        filename (str, optional): filename. Defaults to "data.csv".
+        header (str, optional): header for dataframe. Defaults to "".
+        delimiter (str, optional): delimiter for csv file. Defaults to ",".
+    """
+    if not filename.endswith(".csv"):
+        filename += ".csv"
     data_2D = np.vstack(data_list)
     data_2D = np.transpose(data_2D)
-    np.savetxt(filename, data_2D, delimiter=",", header=header)
-
-
-def test():
-    pass
+    np.savetxt(filename, data_2D, delimiter, header=header)
