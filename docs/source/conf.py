127147d4ea793cbbd9e2fba8a7a9c328af565e1b
slashformotion <slashformotion@protonmail.com>
2020-09-14 13:23:49 +0200

    test readthedoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "sphinx.ext.napoleon"]
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
+napoleon_include_init_with_doc = True
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
 napoleon_use_admonition_for_examples = False
