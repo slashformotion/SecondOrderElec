2d41b1990e685b0e1c7bd8edad8b158f81770592
slashformotion <slashformotion@protonmail.com>
2020-09-14 12:36:38 +0200

    added versionning behavior to setup.py

--- setup.py
+++ setup.py
@@ -6,30 +6,43 @@ tests_require = ["unittest", "sphinx"]
 
 requires = ["numpy", "matplotlib", "scipy"]
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def write_version_py(filename):
+    """Write version."""
+    cnt = """\"""THIS FILE IS GENERATED FROM beem SETUP.PY.\"""
+    version = '%(version)s'
+    """
+    with open(filename, 'w') as a:
+        a.write(cnt % {'version': VERSION})
 
-setup(
-    name="SecondOrderElec",
-    version=VERSION,
-    url="https://github.com/slashformotion/SecondOrderElec",
-    license="MIT",
-    author="slashformotion",
-    author_email="slashformotion@protonmail.com",
-    description="A python module to work with continuous-time system. You will find various second order electronic filters models.",
-    packages=["SecondOrderElec"],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    tests_require=tests_require,
-    install_requires=requires,
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    python_requires=">=3.6",
-)
+if __name__ == "__main__":
+    
+    write_version_py("SecondOrderElec/version.py")
+    
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+
+    setup(
+        name="SecondOrderElec",
+        version=VERSION,
+        url="https://github.com/slashformotion/SecondOrderElec",
+        license="MIT",
+        author="slashformotion",
+        author_email="slashformotion@protonmail.com",
+        description="A python module to work with continuous-time system. You will find various second order electronic filters models.",
+        packages=["SecondOrderElec"],
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        tests_require=tests_require,
+        install_requires=requires,
+        classifiers=[
+            "Development Status :: 2 - Pre-Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+        ],
+        python_requires=">=3.6",
+    )
