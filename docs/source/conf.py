d07479137eb710254b11dd14e11326e0ff17eec3
slashformotion <slashformotion@protonmail.com>
2020-09-14 12:44:19 +0200

    upd conf.py for readthedocs.io

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.abspath('../../.'))
 project = 'SecondOrderElec'
 copyright = '2020, Slashformotion - Vincent Choqueuse'
 author = 'Slashformotion - Vincent Choqueuse'
-
+master_doc = 'index'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
